ddad21942b94441137e267e5e411ce894c0ce62f
mathewgut <mathewgutoskie@gmail.com>
2024-06-30 22:17:07 -0400

    implemented grid resizing

--- script.js
+++ script.js
@@ -2,35 +2,66 @@ const containerDiv = document.querySelector('#container-div');
 const column = document.createElement('div');
 column.setAttribute('id','column');
 
+document.addEventListener('DOMContentLoaded',() => {
+    createGrid()
+});
 
-for(let i = 0; i < 16; i++){
-    const row = document.createElement('div');
-    row.setAttribute('id','row'+i);
-    row.classList.add('row');
+function createGrid(size=16){
+    for(let i = 0; i < size; i++){
+        const row = document.createElement('div');
+        row.setAttribute('id','row'+i);
+        row.classList.add('row');
 
-    console.log(`i ${i}`)
-    for(let j = 0; j < 16; j++){
-        console.log(`j ${j}`);
-        const innerRow = document.createElement('div');
-        innerRow.setAttribute('id','row-inner'+j);
-        row.appendChild(innerRow);
-        innerRow.classList.add('inner');
+        //console.log(`i ${i}`)
+        for(let j = 0; j < size; j++){
+            //console.log(`j ${j}`);
+            const innerRow = document.createElement('div');
+            innerRow.setAttribute('id','row-inner'+ j + '-' + i);
+            row.appendChild(innerRow);
+            innerRow.classList.add('inner');
+        }
+        
+        column.appendChild(row);
+        containerDiv.appendChild(column);
     }
-    
-    column.appendChild(row);
-    containerDiv.appendChild(column);
 }
-
 //document.querySelector('#inner-row').setAttribute('style','border: 3px solid black;')
 
 document.body.appendChild(containerDiv);
 
-containerDiv.addEventListener('mouseover', (e) => {
+column.addEventListener('mouseover', (e) => {
     let target = e.target;
     console.log(target.id);
     const activeObject = document.querySelector(`#${e.target.id}`)
     if(target.id.includes('row-inner')){
-        activeObject.classList.add('active')
+        activeObject.classList.add('active');
     }
 
+})
+
+
+function getUserInput(){
+    let userInput = prompt('Enter a grid size. (e.g 20 would mean 20x20)')
+    userInput = Number(userInput);
+    return userInput;
+}
+
+function nukeGrid(){
+    while (column.firstChild) {
+        console.log(column.firstChild)
+        column.removeChild(column.firstChild);
+    }
+}    
+
+function createUserGrid(){
+    let gridSize = getUserInput();
+    nukeGrid();
+    createGrid(gridSize);
+}
+    
+
+const gridButton = document.querySelector('#alter-grid');
+
+gridButton.addEventListener('click', (e) => {
+    createUserGrid();
 })
